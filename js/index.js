4669c21e7cd64f83782e3a8b1799713fd1ccd28a
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-16 17:15:50 -0700

    added event listener

--- js/index.js
+++ js/index.js
@@ -3,24 +3,45 @@ class FrontEndController {
     this.mainContentScreen = mainContent
     this.mainPostScreen = mainPost
     console.log(this.mainContentScreen, this.mainPostScreen);
+
+    const escKey = {
+      action: 'keydown',
+      el: document,
+      cb: this.keyPressHandler
+    }
+
+    this.addListeners(escKey)
   }
 
+  keyPressHandler(event){
+    if (event.key == "Escape") console.log('nice!');
+  }
 
   showPostScreen(){
-    this.removeClass(this.mainPostScreen,'d-none')
-    setTimeout(()=>this.removeClass(this.mainPostScreen,'d-hide'),300)
+    this.toggleClass(this.mainPostScreen,'d-back')
+    this.toggleClass(document.body,'d-overflow')
+    setTimeout(()=>this.toggleClass(this.mainPostScreen,'d-hide'),200)
   }
 
-  addClass(element, ...args){
-    args.map(style => element.classList.add(style))
+  hidePostScreen(){
+    this.toggleClass(this.mainPostScreen,'d-hide')
+    this.toggleClass(document.body,'d-overflow')
+    setTimeout(()=>this.toggleClass(this.mainPostScreen,'d-none'),200)
   }
-  removeClass(element, ...args){
-    args.map(style => element.classList.remove(style))
+
+  toggleClass(element, ...args){
+    args.map(style => element.classList.toggle(style))
   }
 
-
-
-
+  addListeners(...args){
+    console.log(args[0]);
+    if (args) args.map(listener=>{
+      console.log(listener["el"])
+      console.log(listener.action)
+      console.log(listener.cb);
+      listener["el"].addEventListener(listener.action, listener.cb)
+    })
+  }
 
 
 
