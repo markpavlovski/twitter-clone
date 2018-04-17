1b200475567529c5d560c5a2119f921cccc680ee
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-17 07:50:47 -0700

    working on render feed method"

--- js/index.js
+++ js/index.js
@@ -3,10 +3,8 @@ class FrontEndController {
     this.mainContentScreen = mainContent
     this.mainPostScreen = mainPost
     this.topNav = topNav
-    console.log(this.topNav,this.mainContentScreen, this.mainPostScreen)
-    console.log(this.topNav.querySelector('button'))
 
-    const hidePostSceenEvent = {
+    const keyPressEvents = {
       action: 'keydown',
       el: document,
       cb: this.keyPressHandler.bind(this)
@@ -16,18 +14,85 @@ class FrontEndController {
       el: this.topNav.querySelector('button'),
       cb: this.showPostScreen.bind(this)
     }
+    const clickPostScreenButton = {
+      action: 'click',
+      el: this.mainPostScreen.querySelector('button'),
+      cb: this.submitPost.bind(this)
+    }
+    const clickPostScreenEsc = {
+      action: 'click',
+      el: this.mainPostScreen.querySelector('.post-card-header-x'),
+      cb: ()=>console.log('asd')
+    }
 
-    this.addListeners(hidePostSceenEvent,clickMainScreenButton)
+    this.addListeners(
+      keyPressEvents,
+      clickMainScreenButton,
+      clickPostScreenButton,
+      clickPostScreenEsc
+    )
+
+    this.renderFeed()
   }
 
+
+
+  submitPost(){
+    const textarea = this.mainPostScreen.querySelector('textarea')
+    const name = 'Mark Pavlovski'
+    const handle = '@mrkpvlvski'
+    const timestamp = new Date()
+    const content = textarea.value
+    const tags = content.split(' ').filter(word =>{
+      console.log(word);
+      return word[0]==='#'
+    })
+    const postData = {name, handle, timestamp, content, tags}
+    console.log(postData)
+    console.log('post submitted')
+    // axios post call, with this.renderFeed cb
+    this.hidePostScreen()
+  }
+
+  renderFeed(){
+    // data from server
+    const id = 'XF2RF7D'
+    const name = 'Mark Pavlovski'
+    const handle = '@mrkpvlvski'
+    const timestamp = new Date()
+    const content = 'text'
+    const tags = ['a','b','c']
+    const data = [
+      {id, name, handle, timestamp, content, tags},
+      {id, name, handle, timestamp, content, tags},
+      {id, name, handle, timestamp, content, tags},
+      {id, name, handle, timestamp, content, tags},
+      {id, name, handle, timestamp, content, tags},
+      {id, name, handle, timestamp, content, tags},
+      {id, name, handle, timestamp, content, tags}
+    ]
+    console.log(data);
+  }
+
+
+
+
   keyPressHandler(event){
+
+    const textarea = this.mainPostScreen.querySelector('textarea')
+    const postScreenButton = this.mainPostScreen.querySelector('button')
+    if (textarea.value.length - 1 === 0 || textarea.value.length + 1 === 240) {
+      this.toggleClass(postScreenButton,'active-button')
+    }
     if (event.key == "Escape") this.hidePostScreen()
-    if (event.key == "z") this.showPostScreen()
+
   }
 
   showPostScreen(){
-    console.log('asd');
     if (this.mainPostScreen.classList.contains('d-hide')){
+      const textarea = this.mainPostScreen.querySelector('textarea')
+      textarea.value = ''
+      textarea.focus()
       this.toggleClass(this.mainPostScreen, 'd-back')
       this.toggleClass(document.body, 'd-overflow')
       setTimeout(() => this.toggleClass(this.mainPostScreen, 'd-hide'), 100)
@@ -48,16 +113,9 @@ class FrontEndController {
 
   addListeners(...args){
     if (args) args.map(listener=>{
-      console.log(listener);
       listener["el"].addEventListener(listener.action, listener.cb)
     })
   }
-
-
-
-
-
-
 
 
 }
