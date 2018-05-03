40626429e78feed26616dd122781575a4dd239d8
Mark Pavlovski <markpavlovski@gmail.com>
2018-05-03 16:52:49 -0700

    ready for checkpoint submission

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 class Post {
-  constructor({id, name, handle, timestamp, messageBody, tags}){
+  constructor({
+    id,
+    name,
+    handle,
+    timestamp,
+    messageBody,
+    tags
+  }) {
     this._element = this.createDOMElement(id, name, handle, timestamp, messageBody, tags)
     this.id = id
     this.name = name
@@ -9,10 +16,10 @@ class Post {
     this.tags = tags
   }
 
-  createDOMElement(id, name, handle, timestamp, messageBody, tags){
+  createDOMElement(id, name, handle, timestamp, messageBody, tags) {
 
     // const date = `${timestamp}`.split(' ').filter((el,idx)=> idx === 1 || idx === 2).join(' ')
-    const date = `${new Date(timestamp)}`.split(' ').filter((el,idx)=> idx === 1 || idx === 2).join(' ')
+    const date = `${new Date(timestamp)}`.split(' ').filter((el, idx) => idx === 1 || idx === 2).join(' ')
     const card = document.createElement('div')
     card.setAttribute('postId', id)
     card.classList.add('card')
@@ -36,10 +43,10 @@ class Post {
           <i class="far fa-envelope"></i>
         </p>
       </div>`
-      return card
+    return card
   }
 
-  get element(){
+  get element() {
     return this._element
   }
 
@@ -49,7 +56,7 @@ class Post {
 
 
 class FrontEndController {
-  constructor(topNav, mainContent, mainPost){
+  constructor(topNav, mainContent, mainPost) {
     this.mainContentScreen = mainContent
     this.mainPostScreen = mainPost
     this.topNav = topNav
@@ -82,8 +89,8 @@ class FrontEndController {
     }
     const clickPostScreenEsc = {
       action: 'click',
-      el: this.mainPostScreen.querySelector('.post-card-header-x'),
-      cb: ()=>console.log('asd')
+      el: this.mainPostScreen.querySelector('.post-card-header'),
+      cb: this.hidePostScreen.bind(this)
     }
     const clickMainScreenCard = {
       action: 'click',
@@ -91,7 +98,13 @@ class FrontEndController {
       cb: this.editPost.bind(this)
     }
 
-    this.listenerObjs = {keyPressEvents, clickMainScreenButton, clickPostScreenButton, clickPostScreenEdit, clickPostScreenEsc}
+    this.listenerObjs = {
+      keyPressEvents,
+      clickMainScreenButton,
+      clickPostScreenButton,
+      clickPostScreenEdit,
+      clickPostScreenEsc
+    }
 
     this.addListeners(
       keyPressEvents,
@@ -105,109 +118,113 @@ class FrontEndController {
   }
 
 
-  newPost(){
+  newPost() {
     this.showPostScreen(this.listenerObjs.clickPostScreenButton)
     const heading = this.mainPostScreen.querySelector('.post-card-header-text')
     heading.innerText = 'Create new Post'
     const del = this.mainPostScreen.querySelector('button.post.delete')
-    if (!del.classList.contains('d-hide')) this.toggleClass(del,'d-hide', 'd-back')
+    if (!del.classList.contains('d-hide')) this.toggleClass(del, 'd-hide', 'd-back')
 
   }
 
 
-  editPost(event){
+  editPost(event) {
     const card = event.target.closest('.card')
     if (card) {
       console.log(card);
-      const id = card.getAttribute('postid')
+      const id = card.getAttribute('postId')
       const post = this.posts.find(obj => obj.id === id)
       const textarea = this.mainPostScreen.querySelector('textarea')
       const heading = this.mainPostScreen.querySelector('.post-card-header-text')
       const del = this.mainPostScreen.querySelector('button.post.delete')
       console.log(heading);
       this.showPostScreen(this.listenerObjs.clickPostScreenEdit)
-      textarea.value = post.content
+      textarea.value = post.messageBody
+      textarea.setAttribute('postId',id)
       heading.innerText = 'Edit Post'
-      if (del.classList.contains('d-hide')) this.toggleClass(del,'d-hide', 'd-back')
+      if (del.classList.contains('d-hide')) this.toggleClass(del, 'd-hide', 'd-back')
     }
   }
 
 
-  stagePostData(){
-    console.log("hi");
+  stagePostData() {
     const textarea = this.mainPostScreen.querySelector('textarea')
     const name = 'Mark Pavlovski'
     const handle = '@mrkpvlvski'
-    const timestamp = new Date()
-    const content = textarea.value
-    const tags = content.split(' ').filter(word =>word[0]==='#')
-    return {name, handle, timestamp, content, tags}
+    const messageBody = textarea.value
+    return {
+      name,
+      handle,
+      messageBody
+    }
   }
 
-  editSubmitPost(){
+  editSubmitPost() {
+    const textarea = this.mainPostScreen.querySelector('textarea')
+    const id = textarea.getAttribute('postId')
     const postData = this.stagePostData()
     console.log(postData);
-    // axios post call, with this.renderFeed cb
-    this.hidePostScreen()
-  }
-
-
-  deletePost(){
-    const postData = this.stagePostData()
-    console.log('deleted post')
-    // axios post call, with this.renderFeed cb
-    this.hidePostScreen()
-  }
-
-
-  submitPost(){
-    const postData = this.stagePostData()
-    console.log(postData)
-    console.log('post submitted')
-    // axios post call, with this.renderFeed cb
-    this.hidePostScreen()
-  }
-
-  renderFeed(){
-    axios.get('http://localhost:3000/messages')
-    .then(response => {
-      const data = response.data.data.reverse()
-      const feedContainer = this.mainContentScreen.querySelector('.content-right')
-      while (feedContainer.lastElementChild.classList.contains('card')) {
-        feedContainer.removeChild(feedContainer.lastElementChild)
-      }
-      while (this.posts.length) this.posts.pop()
-      data.map(post=>{
-        const newPost = new Post(post)
-        this.posts.push(newPost)
-        feedContainer.appendChild(newPost.element)
-      })
+    axios.post(`http://localhost:3000/messages/${id}`, postData)
+    .then(()=>{
+      this.renderFeed().then(()=>this.hidePostScreen())
     })
-    .catch(function (error) {
-      console.log(error);
-    });
+  }
 
 
+  deletePost() {
+    const textarea = this.mainPostScreen.querySelector('textarea')
+    const id = textarea.getAttribute('postId')
+    axios.delete(`http://localhost:3000/messages/${id}`)
+    .then(()=>{
+      this.renderFeed().then(()=>this.hidePostScreen())
+    })
+  }
 
 
+  submitPost() {
+    const data = this.stagePostData()
+    axios.post('http://localhost:3000/messages', data)
+    .then(()=>{
+      this.renderFeed().then(()=>this.hidePostScreen())
+    })
+  }
+
+  renderFeed() {
+    return axios.get('http://localhost:3000/messages')
+      .then(response => {
+        const data = response.data.data.reverse()
+        const feedContainer = this.mainContentScreen.querySelector('.content-right')
+        while (feedContainer.lastElementChild.classList.contains('card')) {
+          feedContainer.removeChild(feedContainer.lastElementChild)
+        }
+        while (this.posts.length) this.posts.pop()
+        data.map(post => {
+          const newPost = new Post(post)
+          this.posts.push(newPost)
+          feedContainer.appendChild(newPost.element)
+        })
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
   }
 
 
 
 
-  keyPressHandler(event){
+  keyPressHandler(event) {
 
     const textarea = this.mainPostScreen.querySelector('textarea')
     const postScreenButton = this.mainPostScreen.querySelector('button')
-    if (textarea.value.length - 1 === 0 || textarea.value.length + 1 === 240) {
-      this.toggleClass(postScreenButton,'active-button')
-    }
+    // if (textarea.value.length - 1 === 0 || textarea.value.length + 1 === 240) {
+    //   this.toggleClass(postScreenButton, 'active-button')
+    // }
     if (event.key == "Escape") this.hidePostScreen()
 
   }
 
-  showPostScreen(listener){
-    if (this.mainPostScreen.classList.contains('d-hide')){
+  showPostScreen(listener) {
+    if (this.mainPostScreen.classList.contains('d-hide')) {
       const textarea = this.mainPostScreen.querySelector('textarea')
       textarea.value = ''
       textarea.focus()
@@ -218,27 +235,27 @@ class FrontEndController {
     }
   }
 
-  hidePostScreen(){
-    if (document.body.classList.contains('d-overflow')){
-      this.toggleClass(this.mainPostScreen,'d-hide')
-      this.toggleClass(document.body,'d-overflow')
-      setTimeout(()=>this.toggleClass(this.mainPostScreen,'d-back'),100)
+  hidePostScreen() {
+    if (document.body.classList.contains('d-overflow')) {
+      this.toggleClass(this.mainPostScreen, 'd-hide')
+      this.toggleClass(document.body, 'd-overflow')
+      setTimeout(() => this.toggleClass(this.mainPostScreen, 'd-back'), 100)
       this.removeListeners(this.listenerObjs.clickPostScreenButton, this.listenerObjs.clickPostScreenEdit)
     }
   }
 
-  toggleClass(element, ...args){
+  toggleClass(element, ...args) {
     args.map(style => element.classList.toggle(style))
   }
 
-  addListeners(...args){
-    if (args) args.map(listener=>{
+  addListeners(...args) {
+    if (args) args.map(listener => {
       listener["el"].addEventListener(listener.action, listener.cb)
     })
   }
 
-  removeListeners(...args){
-    if (args) args.map(listener=>{
+  removeListeners(...args) {
+    if (args) args.map(listener => {
       listener["el"].removeEventListener(listener.action, listener.cb)
     })
   }
@@ -249,4 +266,4 @@ class FrontEndController {
 const topNav = document.querySelector('.top-nav')
 const mainContent = document.querySelector('main.main-content')
 const mainPostInterface = document.querySelector('main.post-interface')
-const controller = new FrontEndController(topNav,mainContent,mainPostInterface)
+const controller = new FrontEndController(topNav, mainContent, mainPostInterface)
